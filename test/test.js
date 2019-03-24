314c195ee6d8ffa7c2fa04ea7968143f492ee308
Guillaume Grossetie <ggrossetie@gmail.com>
2019-03-24 17:09:22 +0100

    Apply linter to tests

--- test/test.js
+++ test/test.js
@@ -4,64 +4,63 @@ const optionsConverter = require('../lib/cli.js').convertOptions
 
 describe('Arguments parser', () => {
   it('should parse a command with a single file', () => {
-    let result = argsParser.parse('file.adoc');
-    assert.equal(result['files'].length, 1)
-    assert.equal(result['verbose'], false) // default value
+    const result = argsParser.parse('file.adoc')
+    assert.strictEqual(result['files'].length, 1)
+    assert.strictEqual(result['verbose'], false) // default value
   })
 
   it('should parse a command with the verbose attribute', () => {
-    let result = argsParser.parse('file.adoc --verbose');
-    assert.equal(result['files'].length, 1)
-    assert.equal(result['verbose'], true)
+    const result = argsParser.parse('file.adoc --verbose')
+    assert.strictEqual(result['files'].length, 1)
+    assert.strictEqual(result['verbose'], true)
   })
 
   it('should parse a command with multiple files', () => {
-    let result = argsParser.parse('one.adoc two.adoc');
-    assert.equal(result['files'].length, 2)
-    assert.equal(result['files'].includes('one.adoc'), true)
-    assert.equal(result['files'].includes('two.adoc'), true)
+    const result = argsParser.parse('one.adoc two.adoc')
+    assert.strictEqual(result['files'].length, 2)
+    assert.strictEqual(result['files'].includes('one.adoc'), true)
+    assert.strictEqual(result['files'].includes('two.adoc'), true)
   })
 
   it('should parse a command with multiple attributes', () => {
-    let result = argsParser.parse('one.adoc -a source-highlighter=highlight.js --attribute icons=font@ -a lang=fr');
-    assert.equal(result['files'].length, 1)
-    assert.equal(result['files'].includes('one.adoc'), true)
-    assert.equal(result['attribute'].length, 3)
-    assert.equal(result['attribute'].includes('source-highlighter=highlight.js'), true)
-    assert.equal(result['attribute'].includes('icons=font@'), true)
-    assert.equal(result['attribute'].includes('lang=fr'), true)
+    const result = argsParser.parse('one.adoc -a source-highlighter=highlight.js --attribute icons=font@ -a lang=fr')
+    assert.strictEqual(result['files'].length, 1)
+    assert.strictEqual(result['files'].includes('one.adoc'), true)
+    assert.strictEqual(result['attribute'].length, 3)
+    assert.strictEqual(result['attribute'].includes('source-highlighter=highlight.js'), true)
+    assert.strictEqual(result['attribute'].includes('icons=font@'), true)
+    assert.strictEqual(result['attribute'].includes('lang=fr'), true)
   })
 })
 
 describe('Options converter', () => {
   it('should convert a list of default values to a list of Asciidoctor options', () => {
-    let arguments = argsParser.parse('');
-    let options = optionsConverter(arguments);
-    assert.equal(options['backend'], 'html5')
-    assert.equal(options['doctype'], 'article')
-    assert.equal(options['safe'], 'unsafe')
-    assert.equal(options['header_footer'], true)
-    assert.equal(options['verbose'], 1)
-    assert.equal(options['timings'], false)
-    assert.equal(options['trace'], false)
-    assert.equal(options['mkdirs'], true)
-    assert.equal(options['attributes'], '')
+    const options = optionsConverter(argsParser.parse(''))
+    assert.strictEqual(options['backend'], 'html5')
+    assert.strictEqual(options['doctype'], 'article')
+    assert.strictEqual(options['safe'], 'unsafe')
+    assert.strictEqual(options['header_footer'], true)
+    assert.strictEqual(options['verbose'], 1)
+    assert.strictEqual(options['timings'], false)
+    assert.strictEqual(options['trace'], false)
+    assert.strictEqual(options['mkdirs'], true)
+    assert.strictEqual(options['attributes'], '')
   })
 
   it('should convert a list of default attributes to a list of Asciidoctor options', () => {
-    let arguments = argsParser.parse('one.adoc -a source-highlighter=highlight.js --attribute icons=font@ -a lang=fr');
-    let options = optionsConverter(arguments);
-    assert.equal(options['backend'], 'html5')
-    assert.equal(options['doctype'], 'article')
-    assert.equal(options['safe'], 'unsafe')
-    assert.equal(options['header_footer'], true)
-    assert.equal(options['verbose'], 1)
-    assert.equal(options['timings'], false)
-    assert.equal(options['trace'], false)
-    assert.equal(options['mkdirs'], true)
-    assert.equal(options['attributes'].length, 3)
-    assert.equal(options['attributes'].includes('source-highlighter=highlight.js'), true)
-    assert.equal(options['attributes'].includes('icons=font@'), true)
-    assert.equal(options['attributes'].includes('lang=fr'), true)
+    const args = argsParser.parse('one.adoc -a source-highlighter=highlight.js --attribute icons=font@ -a lang=fr')
+    const options = optionsConverter(args)
+    assert.strictEqual(options['backend'], 'html5')
+    assert.strictEqual(options['doctype'], 'article')
+    assert.strictEqual(options['safe'], 'unsafe')
+    assert.strictEqual(options['header_footer'], true)
+    assert.strictEqual(options['verbose'], 1)
+    assert.strictEqual(options['timings'], false)
+    assert.strictEqual(options['trace'], false)
+    assert.strictEqual(options['mkdirs'], true)
+    assert.strictEqual(options['attributes'].length, 3)
+    assert.strictEqual(options['attributes'].includes('source-highlighter=highlight.js'), true)
+    assert.strictEqual(options['attributes'].includes('icons=font@'), true)
+    assert.strictEqual(options['attributes'].includes('lang=fr'), true)
   })
-});
+})
