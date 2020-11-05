cc59402771ab8cd324e8b60009852404bc49ee8d
Guillaume Grossetie <ggrossetie@gmail.com>
2020-11-05 18:36:08 +0100

    Notify docs.asciidoctor.org

---
+++ scripts/notify.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Send a notification when a successful build occurred on the master branch
+
+set -e
+
+# Trigger a deployment on asciidoctor/docs.asciidoctor.org (using Netlify webhook).
+# The variable "NETLIFY_WEBHOOK_URL" is defined on Travis/GitHub.
+if [[ -z "$NETLIFY_WEBHOOK_URL" ]]; then
+  echo 'The variable NETLIFY_WEBHOOK_URL is not defined, unable to notify Netlify!'
+else
+  curl -X POST -d '' "$NETLIFY_WEBHOOK_URL"
+fi
